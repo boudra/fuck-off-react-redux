2ca2440104548c990ec8a0fd30cc448a0d68c0ee
Mohamed Boudra <boudra.moha@gmail.com>
2020-06-26 09:21:02 +0200

    feat: reducer

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Redux from "redux";
+import {Provider} from "react-redux";
+
 function App() {
   return (
     <div className="App">
