891dd47a595d0c25a68d91af95c0854627d5d9e1
Mohamed Boudra <boudra.moha@gmail.com>
2020-06-26 10:16:59 +0200

    feat: promise middleware

--- src/App.js
+++ src/App.js
@@ -1,28 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
 
-import Redux from "redux";
-import {Provider} from "react-redux";
+import "./App.css";
+
+import reducer from "./reducer";
+import NameForm from "./NameForm";
+import Header from "./Header";
+
+const loggerMiddleware = (store) => (next) => (action) => {
+  console.log("before", store.getState());
+  console.log("action", action);
+  next(action);
+  console.log("after", store.getState());
+};
+
+const promiseMiddleware = (store) => (next) => (action) => {
+  if(action.constructor === Promise) {
+    action.then(newAction => next(newAction));
+  } else {
+    next(action);
+  }
+};
+
+const store = createStore(reducer, applyMiddleware(promiseMiddleware));
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <Header></Header>
+        <NameForm></NameForm>
+      </div>
+    </Provider>
   );
 }
 
--- src/reducer.js
+++ src/reducer.js
@@ -1,7 +1,20 @@
-const initialState = { name: "" };
+const initialState = {
+  title: "What's your name?",
+  subtitle: "",
+  name: ""
+};
 
 export default function (state = initialState, action) {
   switch (action.type) {
+    case "INPUT_NAME":
+      return { ...state, name: action.payload };
+
+    case "SET_TITLE":
+      return { ...state, title: action.payload };
+
+    case "SET_SUBTITLE":
+      return { ...state, subtitle: action.payload };
+
     default:
       return state;
   }
